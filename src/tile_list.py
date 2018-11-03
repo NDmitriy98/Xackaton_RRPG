cbbf9083b92f54d2966172f908fc060860a6d56d
Dmitriy Nakhabin <ndima98@gmail.com>
2018-11-03 23:49:49 +0300

    Параметры _TILE теперь пути к изображениям

--- src/tile_list.py
+++ src/tile_list.py
@@ -1,5 +1,12 @@
+"""
 FLOOR_TILE = '-'
 WALL_TILE = '#'
 ROAD_TILE = 'R'
 BACK_TILE = ' '
 DOOR_TILE = 'D'
+"""
+FLOOR_TILE = 'Drawable/floor.png'
+WALL_TILE = 'Drawable/wall.png'
+ROAD_TILE = 'R'
+BACK_TILE = 'Drawable/back.png'
+DOOR_TILE = 'Drawable/door.png'
