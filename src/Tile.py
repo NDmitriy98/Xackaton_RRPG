2bbbe3fd0db014423624b41b6387d35874f49c0f
Dmitriy Nakhabin <ndima98@gmail.com>
2018-11-03 23:49:10 +0300

    Изменена функция отрисовки для графики Добавлена функция вывода карты в консоль для отладки

--- src/Tile.py
+++ src/Tile.py
@@ -1,11 +1,32 @@
 from src.tile_list import *
+import src.tile_list as tl
+from src.Settings import *
 class Tile:
 
-    def __init__(self, symbol, visible, image=None):
+    def __init__(self, symbol, visible):
         self.symbol = symbol
-        self.image = image
+        self.image = symbol
         self.visible = visible
         self.explored = False
 
-    def draw(self):
-        print(self.symbol, end='')
+    def draw(self,x, y, display, camera):
+        self.image = self.symbol
+        img = pg.image.load(self.image)
+        block = Block(x, y)
+        display.blit(img, camera.apply(block))
+
+    def debug_draw(self):
+        s = ''
+        if self.symbol == tl.FLOOR_TILE:
+            s = '-'
+        elif self.symbol == tl.WALL_TILE:
+            s = '#'
+        elif self.symbol == tl.ROAD_TILE:
+            s = 'R'
+        elif self.symbol == tl.BACK_TILE:
+            s = ' '
+        elif self.symbol == tl.DOOR_TILE:
+            s = 'D'
+        print(s, end='')
+
+
